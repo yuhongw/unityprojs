5662247315c078610a06832020b33ba8e9b2ae35
yu <yuhon@live.com>
2018-06-24 12:41:32 +0800

    init

---
+++ gyroTest/Assets/MGyro.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MGyro : MonoBehaviour
+{
+		
+		//bool gyroBool;
+		Gyroscope gyro;
+		Quaternion quatMult;
+		Quaternion quatMap;
+		// Label ul;
+		
+		GameObject camParent;
+		void Awake()
+		{
+			
+			// find the current parent of the camera's transform
+			Transform currentParent = transform.parent;
+			// instantiate a new transform
+			camParent = new GameObject("camParent");
+			// match the transform to the camera position
+			camParent.transform.position = transform.position;
+			// make the new transform the parent of the camera transform
+			transform.parent = camParent.transform;
+			// make the original parent the grandparent of the camera transform
+			//camParent.transform.parent = currentParent;
+			// instantiate a new transform
+			GameObject camGrandparent = new GameObject("camGrandParent");
+			// match the transform to the camera position
+			camGrandparent.transform.position = transform.position;
+			// make the new transform the parent of the camera transform
+			camParent.transform.parent = camGrandparent.transform;
+			// make the original parent the grandparent of the camera transform
+			camGrandparent.transform.parent = currentParent;
+			
+			//gyroBool = true;
+			//if (gyroBool) {
+			gyro = Input.gyro;
+			//ul = GameObject.Find("Rotation").GetComponent<UILabel>();
+			
+			gyro.enabled = true;
+			camParent.transform.eulerAngles = new Vector3(90,0, 0);
+			quatMult = new Quaternion(0, 0, 1, 0);
+			
+		}
+		
+		void Update()
+		{
+			quatMap = new Quaternion(gyro.attitude.x, gyro.attitude.y, gyro.attitude.z, gyro.attitude.w);
+			Quaternion qt=quatMap * quatMult;
+			transform.localRotation =qt;
+		}
+		
+	}
+
---
+++ gyroTest/Assets/WebCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour {
+
+	public WebCamTexture cameraTexture;
+	public string cameraName = "";
+	public RawImage rawimage;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Test());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator Test()
+	{
+		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+		if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+		{
+			WebCamDevice[] devices = WebCamTexture.devices;
+			cameraName = devices[0].name;
+			cameraTexture = new WebCamTexture(cameraName, 1440, 720,15);
+
+			rawimage.texture = cameraTexture;
+			cameraTexture.Play();
+
+		}
+	}
+}
